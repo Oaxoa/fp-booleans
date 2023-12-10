6011505b7f1084b0b64ef99a8f7d1ba7cd973f17
ppesenti-rbi <ppesenti@rbi.com>
2023-12-10 17:32:12 +0100

    add semicolons back

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-export type TBooleanPredicate = (...params: any[]) => boolean
-export type THigherOrderBooleanPredicate = (...params: any[]) => TBooleanPredicate
-type TNotArg = boolean | TBooleanPredicate | THigherOrderBooleanPredicate
+export type TBooleanPredicate = (...params: any[]) => boolean;
+export type THigherOrderBooleanPredicate = (...params: any[]) => TBooleanPredicate;
+type TNotArg = boolean | TBooleanPredicate | THigherOrderBooleanPredicate;
 
 const isTBooleanPredicate = (
 	arg: TBooleanPredicate | THigherOrderBooleanPredicate
-): arg is TBooleanPredicate => typeof arg(0) === 'boolean'
+): arg is TBooleanPredicate => typeof arg(0) === 'boolean';
 
-export function not(value: boolean): boolean
-export function not(f: TBooleanPredicate): TBooleanPredicate
-export function not(f: THigherOrderBooleanPredicate): THigherOrderBooleanPredicate
+export function not(value: boolean): boolean;
+export function not(f: TBooleanPredicate): TBooleanPredicate;
+export function not(f: THigherOrderBooleanPredicate): THigherOrderBooleanPredicate;
 export function not(arg: TNotArg): TNotArg {
 	if (typeof arg === 'function') {
 		if (isTBooleanPredicate(arg)) {
-			return (...args: any[]) => !arg(...args)
+			return (...args: any[]) => !arg(...args);
 		}
-		return (...higherOrderArgs: any[]) => not(arg(...higherOrderArgs))
+		return (...higherOrderArgs: any[]) => not(arg(...higherOrderArgs));
 	}
-	return !arg
+	return !arg;
 }
 
-type TMergeMethodType = typeof Array.prototype.every | typeof Array.prototype.some
+type TMergeMethodType = typeof Array.prototype.every | typeof Array.prototype.some;
 const merge =
 	(method: TMergeMethodType) =>
 	(...args: boolean[] | TBooleanPredicate[]): boolean | TBooleanPredicate => {
@@ -27,19 +27,19 @@ const merge =
 			return (...predicatesArgs: any[]) =>
 				method.call(args as TBooleanPredicate[], (f: TBooleanPredicate) =>
 					f(...predicatesArgs)
-				)
+				);
 		}
-		return method.call(args as boolean[], Boolean)
-	}
+		return method.call(args as boolean[], Boolean);
+	};
 
-export function and(...args: boolean[]): boolean
-export function and(...args: TBooleanPredicate[]): TBooleanPredicate
+export function and(...args: boolean[]): boolean;
+export function and(...args: TBooleanPredicate[]): TBooleanPredicate;
 export function and(...args: boolean[] | TBooleanPredicate[]): boolean | TBooleanPredicate {
-	return merge(Array.prototype.every)(...args)
+	return merge(Array.prototype.every)(...args);
 }
 
-export function or(...args: boolean[]): boolean
-export function or(...args: TBooleanPredicate[]): TBooleanPredicate
+export function or(...args: boolean[]): boolean;
+export function or(...args: TBooleanPredicate[]): TBooleanPredicate;
 export function or(...args: boolean[] | TBooleanPredicate[]): boolean | TBooleanPredicate {
-	return merge(Array.prototype.some)(...args)
+	return merge(Array.prototype.some)(...args);
 }
--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,8 @@
 const config = {
 	trailingComma: 'es5',
 	tabWidth: 4,
-	semi: false,
 	singleQuote: true,
 	useTabs: true,
 	printWidth: 95,
-}
-export default config
+};
+export default config;

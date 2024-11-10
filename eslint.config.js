4d30e348df062f470580f828627fe07496ae10d4
Pierluigi Pesenti <ppesenti@rbi.com>
2024-11-10 13:31:04 +0700

    Rework eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,27 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import PluginImport from 'eslint-plugin-import';
 
 export default [
-	{
-		ignores: ['coverage/', 'dist/', 'assets/', '.github/', 'node_modules/'],
-	},
 	js.configs.recommended,
 	...tseslint.configs.recommended,
 	eslintConfigPrettier,
 	{
+		ignores: ['coverage/', 'dist/', 'assets/', '.github/', 'node_modules/'],
+	},
+	{
+		languageOptions: {
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
 		plugins: { import: PluginImport },
 		rules: {
+			'no-console': 'off',
 			'import/no-unresolved': 'off',
 			'@typescript-eslint/ban-types': 'off',
 			'@typescript-eslint/ban-ts-comment': 'off',
